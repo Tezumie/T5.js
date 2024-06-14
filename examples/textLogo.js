fb72c1e8b49e263064ea20e02b51792a840c8f9a
Tezumie <102488626+Tezumie@users.noreply.github.com>
2024-06-14 11:21:45 -0600

    Update textLogo.js

--- examples/textLogo.js
+++ examples/textLogo.js
@@ -1,26 +1,56 @@
-let aspectRatio = 1 / 1;
+let aspectRatio = 1.33;
 
 function setup() {
     createCanvas(window.innerWidth, window.innerWidth / aspectRatio);
-    flexibleCanvas(1000); // canvas is 1000 units wide
+    flexibleCanvas(1430); // canvas is 1000 units wide
 }
 
 function draw() {
-    background('#8947d8');
+    background('#121212');
     noStroke();
-    fill(255);
-
+    fill('#917eff');
+    textStyle('bold');
     textSize(400);
     textAlign('center', 'center');
     textFont('Helvetica');
 
-    text('T', 140, 650);
-    text('5', 350, 650);
+    text('T', 340, 700);
+    text('5', 550, 700);
+    fill('#d0ff00');
+    text('J', 860, 700);
+    text('S', 1090, 700);
 
-    text('J', 660, 650);
-    text('S', 860, 650);
-
+    fill('#ffffff');
     textSize(300);
     textFont('Times New Roman');
-    text('*', 550, 590);
+    text('*', 750, 640);
 }
+
+
+// let aspectRatio = 1.33;
+
+// function setup() {
+//     createCanvas(window.innerWidth, window.innerWidth / aspectRatio);
+//     flexibleCanvas(1400); // canvas is 1000 units wide
+// }
+
+// function draw() {
+//     background('#091725');
+//     noStroke();
+//     fill('#3377bb');
+//     textStyle('bold');
+//     textSize(400);
+//     textAlign('center', 'center');
+//     // textFont('Helvetica');
+
+//     text('p', 330, 620);
+//     text('5', 540, 620);
+//     fill('#ffbb33');
+//     text('p', 890, 620);
+//     text('y', 1090, 620);
+
+//     textSize(300);
+//     fill('#ffffff');
+//     // textFont('Times New Roman');
+//     text('*', 720, 750);
+// }
