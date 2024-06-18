f1586989dd744188e3825ada79f86cd2081afbbb
Tezumie <102488626+Tezumie@users.noreply.github.com>
2024-06-18 11:16:38 -0600

    add fillArea(x,y,color)

--- dev/dev.js
+++ dev/dev.js
@@ -1,12 +1,24 @@
 let aspectRatio = 3 / 4;
 
 function setup() {
-  createCanvas(window.innerWidth, window.innerWidth / aspectRatio);
-  // Scale all drawing operations automatically to any window size
-  flexibleCanvas(1000); // Canvas is 1000 units wide and Dimension Agnostic
+  canvas = createCanvas(window.innerWidth, window.innerWidth / aspectRatio);
+  flexibleCanvas(1000);
+  noLoop();
+  noSmooth();
+  // noStroke();
+  strokeWeight(3)
 }
 
 function draw() {
-  background(220);
-  rect(0, 0, 500) // 500x500 units is half the width 
+  fill(color(255,150))
+  ellipse(0, 0, 300);
+  ellipse(width / 2, height / 5, width / 1.5);
+  ellipse(width / 2, height, width / 1.5);
+  ellipse(0, height / 1.7, width / 1.2);
+  ellipse(width, height / 3, width / 2);
+
+  fillArea(203, 5, color(2, 115, 214));
+  fillArea(width / 2, height / 2, color(66, 214, 29));
+  fillArea(width - 5, 5, color(214, 29, 29));
+  fillArea(5, height - 10, color(253, 216, 6));
 }
