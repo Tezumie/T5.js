c948b374b9aace15aad072e574ccb05193c99b6a
Tezumie <102488626+Tezumie@users.noreply.github.com>
2024-07-20 15:37:08 -0600

    T5 Structure overhaul

--- dev/dev.js
+++ dev/dev.js
@@ -1,24 +1,84 @@
-let aspectRatio = 3 / 4;
+// let angle = 0;
+// function setup() {
+//     createCanvas(1400, 1400);
+//     angle = (3.1415926535 / 4);
+//     strokeWeight(3);
+//     stroke('#008709');
+
+// }
+// function draw() {
+//     background('#ffffff');
+//     translate((width / 2), height);
+//     branch(400);
+// }
+// function branch(len) {
+//     line(0, 0, 0, (len * -1));
+//     translate(0, (len * -1));
+//     if((len > 4)){
+//         push();
+//         rotate(angle);
+//         branch((len * 0.67));
+//         pop();
+//         push();
+//         rotate((angle * -1));
+//         branch((len * 0.67));
+//         pop();
+//     }
+//     else{
+//         return ;
+//     }
+// }
+
+let img, Canvas;
 
 function setup() {
-  canvas = createCanvas(window.innerWidth, window.innerWidth / aspectRatio);
-  flexibleCanvas(1000);
-  noLoop();
-  noSmooth();
-  // noStroke();
-  strokeWeight(3)
+    Canvas = createCanvas(800, 800);
+    background(255);
+    noLoop();
 }
 
 function draw() {
-  fill(color(255,150))
-  ellipse(0, 0, 300);
-  ellipse(width / 2, height / 5, width / 1.5);
-  ellipse(width / 2, height, width / 1.5);
-  ellipse(0, height / 1.7, width / 1.2);
-  ellipse(width, height / 3, width / 2);
+    createSimplePattern();
+    createPattern();
 
-  fillArea(203, 5, color(2, 115, 214));
-  fillArea(width / 2, height / 2, color(66, 214, 29));
-  fillArea(width - 5, 5, color(214, 29, 29));
-  fillArea(5, height - 10, color(253, 216, 6));
+    background(255);
+
+    // Draw and clip the pattern to a rectangle
+    rect(width / 2 - width / 4, 0, width / 2, height / 2);
+    beginPattern();
+    image(img, 0, 0);
+    endPattern();
+
+    // Draw and clip the pattern to an ellipse
+    ellipse(width / 2, height - width / 4, width / 2);
+    beginPattern();
+    image(img, 0, 0);
+    endPattern();
 }
+
+function createPattern() {
+    img = createGraphics(width, height);
+    img.copy(Canvas, 0, 0, width, height, 0, 0, img.width, img.height);
+    clear();
+}
+
+function beginPattern() {
+    drawingContext.save();
+    drawingContext.clip();
+}
+
+function endPattern() {
+    drawingContext.restore();
+}
+
+function createSimplePattern() {
+    stroke(0);
+    strokeWeight(2);
+    for (let x = 0; x < width; x += 20) {
+        for (let y = 0; y < height; y += 20) {
+            line(x, y, x + 10, y + 10);
+            line(x + 10, y, x, y + 10);
+        }
+    }
+}
+
