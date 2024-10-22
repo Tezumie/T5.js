26082b8a6d359c3582f11a45a24e4307f17b711f
Tezumie <102488626+Tezumie@users.noreply.github.com>
2024-10-22 13:18:20 -0600

    add norm(value, start, stop), adjust T5.prototype.registerMethod.

--- T5Main.js
+++ T5Main.js
@@ -19,7 +19,10 @@ T5.prototype.registerPreloadMethod = (methodName, functionObject) => {
     T5.prototype[methodName] = functionObject[methodName];
 };
 
-T5.prototype.registerMethod = (methodName, functionObject) => {
+T5.prototype.registerMethod = function (methodName, functionObject) {
+    if (!T5.methods[methodName]) {
+        T5.methods[methodName] = [];
+    }
     T5.methods[methodName].push(functionObject);
 };
 
@@ -143,6 +146,7 @@ function T5(scope = 'global', parent) {
     $.start = () => {
 
         if (typeof $.preload === 'function') {
+            bindGlobals($)
             $.preload();
         }
         // Check if preload function exists and if there are items to preload
@@ -152,10 +156,6 @@ function T5(scope = 'global', parent) {
 
             if ($.frameCount === 0 && $.context === null) $.createCanvas(100, 100);
             $.resetMatrix();
-            for (let initMethod of T5.methods.init) {
-                initMethod.call($);
-            }
-
             $._shouldDrawOnce = true;
             requestAnimationFrame(() => {
                 _draw();
@@ -170,10 +170,6 @@ function T5(scope = 'global', parent) {
 
                     if ($.frameCount === 0 && $.context === null) $.createCanvas(100, 100);
                     $.resetMatrix();
-                    for (let initMethod of T5.methods.init) {
-                        initMethod.call($);
-                    }
-
                     $._shouldDrawOnce = true;
                     requestAnimationFrame(() => {
                         _draw();
@@ -195,10 +191,27 @@ function T5(scope = 'global', parent) {
 
     document.addEventListener('DOMContentLoaded', $.start);
 
+    if (T5.methods.init) {
+        for (let initMethod of T5.methods.init) {
+            if (typeof initMethod === 'function') {
+                initMethod.call($);
+            }
+        }
+    }
+
     for (let m in T5.addOns) {
         T5.addOns[m]($, proxy, globalScope);
     }
 
+    function bindGlobals($) {
+        for (let key in $) {
+            if ($[key] instanceof Function) {
+                window[key] = $[key].bind($);
+            } else {
+                window[key] = $[key];  // Bind non-function properties too
+            }
+        }
+    }
 
     if ($._globalSketch) {
         globalScope.T5 = T5;
@@ -230,7 +243,6 @@ if (typeof window === 'object') {
         }
     });
 }
-
 //************************************************************************//
 //*******************************-T5Cenvas-*******************************//
 //************************************************************************//
@@ -2018,6 +2030,7 @@ T5.addOns.math = ($, p) => {
     $.degrees = (radians) => radians * (180 / Math.PI);
     $.radians = (degrees) => degrees * (Math.PI / 180);
     $.map = (value, start1, stop1, start2, stop2) => start2 + (stop2 - start2) * ((value - start1) / (stop1 - start1));
+    $.norm = (value, start, stop) => (value - start) / (stop - start);
     $.lerp = (start, stop, amt) => start + (stop - start) * amt;
     $.int = (n) => n < 0 ? Math.ceil(n) : Math.floor(n);
     $.dist = (x1, y1, x2, y2) => {
